3c7f54b66d018507bd6e1b697cfaf76879e79896
Yerlan <erlantemirbolat87@gmail.com>
2022-01-28 01:19:48 +0600

    Add image profile

--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -8,14 +8,44 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "plus_photo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -31,7 +61,7 @@ class ViewController: UIViewController {
     @objc func handleTextInputChange() {
         
         let isFormValid: Bool = emailTextField.text?.count ?? 0 > 0 &&
-                                userTextField.text?.count ?? 0 > 0 &&
+                                usernameTextField.text?.count ?? 0 > 0 &&
                                 passwordTextField.text?.count ?? 0 > 0
         
         if isFormValid {
@@ -43,7 +73,7 @@ class ViewController: UIViewController {
         }
     }
     
-    let userTextField: UITextField = {
+    let usernameTextField: UITextField = {
         let tf = UITextField()
         tf.placeholder = "Username"
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
@@ -81,7 +111,7 @@ class ViewController: UIViewController {
     @objc func handleSignUp() {
         
         guard let email = emailTextField.text, email.count > 0 else { return }
-        guard let user = userTextField.text, user.count > 0 else { return }
+        guard let username = usernameTextField.text, username.count > 0 else { return }
         guard let password = passwordTextField.text, password.count > 0 else { return }
         
         Auth.auth().createUser(withEmail: email, password: password, completion: { user, error in
@@ -91,7 +121,44 @@ class ViewController: UIViewController {
                 return
             }
             
-            print("Successfully created user:", user)
+            print("Successfully created user:", user?.user.uid ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            
+            guard let uploadData = image.jpegData(compressionQuality: 0.3) else { return }
+                        
+            let storageRef = Storage.storage().reference()
+            let storageRefChild = storageRef.child("user_profile_pictures/\(user?.user.uid).jpg")
+            storageRefChild.putData(uploadData, metadata: nil, completion: { (metadata, err) in
+                if let err = err {
+                    print("Unable to upload image into storage due to: \(err)")
+                }
+                
+                storageRefChild.downloadURL(completion: { (url, err) in
+                    if let err = err {
+                        print("Unable to retrieve URL due to error: \(err.localizedDescription)")
+                        return
+                    }
+                    let profilePicUrl =  url?.absoluteString
+                    print("Profile Image successfully uploaded into storage with url: \(profilePicUrl ?? "" )")
+                    
+                    guard let uid = user?.user.uid else { return }
+                    
+                    let dictionaryValues = ["username": username, "profileImageUrl": profilePicUrl]
+                    let values = [uid: dictionaryValues]
+                    
+                    Database.database().reference().child("USER-INFO").updateChildValues(values) { err, ref in
+                        
+                        if let err = err {
+                            print("Failed to save user into db:", err)
+                            return
+                        }
+                        
+                        print("Successfully saved user into to db.")
+                    }
+                })
+            })
+            
         })
     }
 
@@ -109,7 +176,7 @@ class ViewController: UIViewController {
 
     fileprivate func setupInputFields() {
         
-        let stackView = UIStackView(arrangedSubviews: [emailTextField, userTextField, passwordTextField, signUpButton])
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, usernameTextField, passwordTextField, signUpButton])
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.spacing = 10
